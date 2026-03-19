b832ad90b8df2bed4a9cb75b389ffea15ca268d1
Sohaila <sohaila.hesham456@gmail.com>
2026-03-19 23:18:11 +0200

    feat: add data fetching, search, and filter to categories, products, and suppliers pages

---
+++ js/pages/products.js
@@ -0,0 +1,181 @@
+import renderTable from "../components/table.js";
+import {
+  fetchData,
+  postData,
+  updateData,
+  deleteData,
+} from "../services/api.js";
+
+let products = [];
+let categories = [];
+let suppliers = [];
+
+export async function loadProducts() {
+  await loadData();
+  renderProducts();
+  setupEventListeners();
+}
+
+async function loadData() {
+  products = await fetchData("products");
+  categories = await fetchData("categories");
+  suppliers = await fetchData("suppliers");
+}
+
+//* render the whole html of products page
+function renderProducts() {
+  let categoryOptions = categories
+    .map((c) => `<option value="${c.id}">${c.name}</option>`)
+    .join("");
+
+  let html = `
+    <!-- Search Bar -->
+    <div class="d-flex gap-1 mb-3 align-items-center p-3 bg-white rounded border">
+      <i class="bi bi-search text-muted"></i>
+      <input type="text" id="searchProd" placeholder="Search products..." 
+        class="form-control form-control-sm border-0 shadow-none">
+      <select id="categoryFilter" class="form-select form-select-sm border-0 shadow-none" style="width: 160px;">
+        <option value="">All Categories</option>
+        ${categoryOptions}
+      </select>
+      <select id="statusFilter" class="form-select form-select-sm border-0 shadow-none" style="width: 130px;">
+        <option value="">All Status</option>
+        <option value="in">In Stock</option>
+        <option value="low">Low Stock</option>
+        <option value="out">Out of Stock</option>
+      </select>
+      <button class="btn btn-primary btn-sm px-3 ms-auto w-auto" id="addProductBtn">
+        <i class="bi bi-plus-lg"></i> Add Product
+      </button>
+    </div>
+
+    <div id="searchStats" class="mb-2 small text-muted"></div>
+    
+    <!-- Table Container -->
+    <div id="productsTableContainer">
+      ${getTableHtml()}
+    </div>
+  `;
+
+  document.getElementById("pageContent").innerHTML = html;
+}
+
+//* to bring the table whatever there is a filter/ search/ all prods
+function getTableHtml(filteredProducts = products) {
+  let tableData = filteredProducts.map((p) => ({
+    id: p.id,
+    sku: `<span class="sku-badge">${p.sku}</span>`,
+    name: p.name,
+    category: getCategoryName(p.categoryId),
+    supplier: getSupplierName(p.supplierId),
+    price: p.price,
+    quantity: p.quantity,
+    status: getStatus(p.quantity),
+  }));
+
+  let columns = [
+    "sku",
+    "name",
+    "category",
+    "supplier",
+    "price",
+    "quantity",
+    "status",
+  ];
+
+  return renderTable(tableData, columns);
+}
+
+//* for all event listeners
+function setupEventListeners() {
+  //& Search
+  document.getElementById("searchProd")?.addEventListener("input", function () {
+    filterProducts();
+  });
+
+  //& Filter by category
+  document
+    .getElementById("categoryFilter")
+    ?.addEventListener("change", function () {
+      filterProducts();
+    });
+
+  //& Filter by status
+  document
+    .getElementById("statusFilter")
+    ?.addEventListener("change", function () {
+      filterProducts();
+    });
+}
+
+//* Filter and Search function
+function filterProducts() {
+  let searchTerm = document.getElementById("searchProd").value.toLowerCase();
+  let categoryId = document.getElementById("categoryFilter").value;
+  let statusFilter = document.getElementById("statusFilter").value;
+
+  let filtered = products.filter((p) => {
+    //^ Search by (sku, name, category, supplier)
+    if (searchTerm) {
+      let matches =
+        p.name.toLowerCase().includes(searchTerm)
+        || p.sku.toLowerCase().includes(searchTerm)
+        || getCategoryName(p.categoryId).toLowerCase().includes(searchTerm)
+        || getSupplierName(p.supplierId).toLowerCase().includes(searchTerm);
+      if (!matches) return false;
+    }
+
+    //^ Filter by Category
+    if (categoryId && p.categoryId != categoryId) return false;
+
+    //^ Filter by status
+    if (statusFilter) {
+      if (statusFilter === "out" && p.quantity > 0) return false;
+      if (statusFilter === "low" && (p.quantity <= 0 || p.quantity > 5))
+        return false;
+      if (statusFilter === "in" && p.quantity <= 5) return false;
+    }
+
+    return true;
+  });
+
+  //& Change Table data
+  document.getElementById("productsTableContainer").innerHTML =
+    getTableHtml(filtered);
+
+  //& update status
+  updateStats(filtered.length, searchTerm, categoryId, statusFilter);
+}
+
+//* stats
+function updateStats(count, searchTerm, categoryId, statusFilter) {
+  let statsDiv = document.getElementById("searchStats");
+  if (!statsDiv) return;
+
+  if (searchTerm || categoryId || statusFilter) {
+    statsDiv.innerHTML = `Found ${count} product${count !== 1 ? "s" : ""}`;
+  } else {
+    statsDiv.innerHTML = "";
+  }
+}
+
+//* get category name
+function getCategoryName(id) {
+  let cat = categories.find((e) => e.id == id);
+  return cat ? cat.name : "undefined";
+}
+
+//* get supplier name
+function getSupplierName(id) {
+  let sup = suppliers.find((e) => e.id == id);
+  return sup ? sup.name : "undefined";
+}
+
+//* get status of products
+function getStatus(quantity) {
+  if (quantity <= 0)
+    return `<span class="status-badge status-out">Out of stock</span>`;
+  else if (quantity <= 5)
+    return `<span class="status-badge status-low">Low stock</span>`;
+  else return `<span class="status-badge status-in">In stock</span>`;
+}
---
+++ js/pages/suppliers.js
@@ -0,0 +1,119 @@
+import renderTable from "../components/table.js";
+import {
+  fetchData,
+  postData,
+  updateData,
+  deleteData,
+} from "../services/api.js";
+
+let products = [];
+let categories = [];
+let suppliers = [];
+
+export async function loadSuppliers() {
+  await loadData();
+  renderSuppliers();
+  setupEventListeners();
+}
+
+async function loadData() {
+  products = await fetchData("products");
+  categories = await fetchData("categories");
+  suppliers = await fetchData("suppliers");
+}
+
+//* render the whole html of Suppliers page
+function renderSuppliers() {
+  let html = `
+  <div class="d-flex gap-2 mb-3 align-items-center p-3 bg-white rounded border">
+    <i class="bi bi-search text-muted"></i>
+    <input type="text" id="searchSup" placeholder="Search suppliers..." 
+      class="form-control form-control-sm border-0 shadow-none">
+
+    <button class="btn btn-primary btn-sm px-3 ms-auto text-nowrap" id="addSupplierBtn">
+      <i class="bi bi-plus-lg"></i> Add Supplier
+    </button>
+  </div>
+  
+  <div id="searchStats" class="mb-2 small text-muted"></div>
+  
+  <div id="suppliersTableContainer">
+      ${getTableHtml()}
+  </div>
+`;
+
+  document.getElementById("pageContent").innerHTML = html;
+}
+
+//* to bring the table whatever there is a filter/ search
+function getTableHtml(filteredSuppliers = suppliers) {
+  let tableData = filteredSuppliers.map((s) => ({
+    id: s.id,
+    name: s.name,
+    contact: s.contact || "-",
+    email: s.email || "-",
+    phone: s.phone || "-",
+    address: s.address || "-",
+    products: getProductsNumber(s.id),
+  }));
+
+  let columns = [
+    "id",
+    "name",
+    "contact",
+    "email",
+    "phone",
+    "address",
+    "products",
+  ];
+
+  return renderTable(tableData, columns);
+}
+
+//* for all event listeners
+function setupEventListeners() {
+  document
+    .getElementById("searchSup")
+    ?.addEventListener("input", filterSuppliers);
+}
+
+//* search function
+function filterSuppliers() {
+  let searchTerm = document.getElementById("searchSup").value.toLowerCase();
+
+  //^ Search by ( name, contact, email, phone, address)
+  let filtered = suppliers.filter((s) => {
+    return (
+      s.name.toLowerCase().includes(searchTerm)
+      || (s.contact && s.contact.toLowerCase().includes(searchTerm))
+      || (s.email && s.email.toLowerCase().includes(searchTerm))
+      || (s.phone && s.phone.includes(searchTerm))
+      || (s.address && s.address.toLowerCase().includes(searchTerm))
+    );
+  });
+
+  //& Update Table
+  document.getElementById("suppliersTableContainer").innerHTML =
+    getTableHtml(filtered);
+
+  //& Update stats
+  updateStats(filtered.length, searchTerm);
+}
+
+//* stats
+function updateStats(count, searchTerm) {
+  let statsDiv = document.getElementById("searchStats");
+  if (!statsDiv) return;
+
+  if (searchTerm) {
+    statsDiv.innerHTML = `Found ${count} supplier${count !== 1 ? "s" : ""} matching "${searchTerm}"`;
+  } else {
+    statsDiv.innerHTML = "";
+  }
+}
+
+//* get products count for supplier
+function getProductsNumber(id) {
+  let count = products.filter((p) => p.supplierId == id).length;
+  return `<span class="badge rounded-pill px-2" style="background:#eff6ff; color:#3b82f6;">${count}</span>`;
+}
